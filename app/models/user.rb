12fd40b089678052e98f2d22a16744e69bc33fbd
Austin Ratcliff <hello.austinratcliff@gmail.com>
2018-10-17 23:03:55 -0500

    Add user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :orders
+  has_many :messages
+
   def self.full_name(user)
     user.first_name.capitalize + ' ' + user.last_name.capitalize
   end
