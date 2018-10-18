314bd89d6b2aa97731157f5625e98e5ef848a6d5
Austin Ratcliff <hello.austinratcliff@gmail.com>
2018-10-18 17:47:55 -0500

    Add basic functionality to pickups new/create/edit/update actions

--- app/controllers/pickups_controller.rb
+++ app/controllers/pickups_controller.rb
@@ -1,13 +1,41 @@
 class PickupsController < ApplicationController
   def new
+    @order = Order.find(params[:order_id])
+    @pickup = Pickup.new
   end
 
   def create
+    @order = Order.find(params[:order_id])
+    @pickup = Pickup.new(pickup_params)
+    @pickup.order_id = @order.id
+
+    if @pickup.save
+      @order.delivery.destroy if @order.delivery
+      redirect_to @order
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @order = Order.find(params[:order_id])
+    @pickup = Pickup.find(params[:id])
   end
 
   def update
+    @order = Order.find(params[:order_id])
+    @pickup = Pickup.find(params[:id])
+
+    if @pickup.update_attributes(pickup_params)
+      redirect_to @order
+    else
+      render 'edit'
+    end
   end
+
+  private
+
+    def pickup_params
+      params.require(:pickup).permit(:pickup_at)
+    end
 end
