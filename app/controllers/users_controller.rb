c13527c8d8146f642833cbae6ed4ec54df236a1f
Austin Ratcliff <hello.austinratcliff@gmail.com>
2018-10-17 22:52:22 -0500

    Add basic functionality to users new/create/show/edit/update/destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,48 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    @user.name = User.full_name(@user)
+
+    if @user.save
+      log_in(@user)
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      @user.update_attributes(name: User.full_name(@user))
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    User.find(params[:id]).destroy
+    log_out
+    redirect_to root_path
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :phone, :password, :password_confirmation)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  def self.full_name(user)
+    user.first_name.capitalize + ' ' + user.last_name.capitalize
+  end
 end
