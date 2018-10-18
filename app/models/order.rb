824115636f89404e7efe91bb5ce61ea4880f9918
Austin Ratcliff <hello.austinratcliff@gmail.com>
2018-10-18 15:08:40 -0500

    Add orders show view skeleton

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,25 @@
 class Order < ApplicationRecord
+  def self.view_count(order)
+    counts = [
+      [6, 'Half dozen'],
+      [12, 'One dozen'],
+      [24, 'Two dozens'],
+      [36, 'Three dozens'],
+      [nil, "Let's chat"]
+    ]
+
+    count = counts.find do |count|
+      order.count == count.first
+    end
+
+    count.last
+  end
+
+  def self.view_is_delivery(order)
+    if order.is_delivery
+      'Yes'
+    else
+      'No'
+    end
+  end
 end
