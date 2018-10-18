ed7903d7e46c550598a681fcc4d5d718a209ed33
Austin Ratcliff <hello.austinratcliff@gmail.com>
2018-10-17 23:14:25 -0500

    Add basic functionality to sessions create/destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:email])
+
+    if user && user.authenticate(params[:password])
+      log_in(user)
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def destroy
+    log_out
+    redirect_to root_path
   end
 end
