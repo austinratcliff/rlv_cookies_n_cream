71ed554fd38d4eefbc0007d4c5411f9410196748
Austin Ratcliff <hello.austinratcliff@gmail.com>
2018-10-18 22:36:30 -0500

    Add basic functionality to messages new/create actions

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,24 @@
 class MessagesController < ApplicationController
   def new
+    @message = Message.new
   end
 
   def create
+    @message = Message.new(message_params)
+    @message.user_id = current_user.id
+
+    if @message.save
+      flash[:sent] = 'Your message was sent!'
+      redirect_to root_path
+    else
+      flash.now[:not_sent] = 'Your message was not sent — try again!'
+      render 'new'
+    end
   end
+
+  private
+
+    def message_params
+      params.require(:message).permit(:body)
+    end
 end
